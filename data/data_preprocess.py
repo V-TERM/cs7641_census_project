d43efc00cc1186c1a61cd5644626aa9a31b03878
Monish <monish.ramadoss@outlook.com>
2021-11-15 22:37:26 -0500

    added preprocessing to main file

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -11,18 +11,20 @@ class Presidential_Results(object):
         self._county_block = pd.read_csv(join(root_dir, 'pres_cnty.csv'))
         self._state_block = pd.read_csv(join(root_dir, 'pres_state.csv'))
         self._census_data = pd.read_csv(join(root_dir, 'use_later.csv'))
-
+        
     @property
     def counties(self):
-        return self._county_block['county'].to_list()
+        return [tuple(r) for r in self._county_block[['county', 'state']].to_numpy()]
 
     @property
     def states(self):
         return self._state_block['state'].to_list()
 
-    def get_county(self, cnty):
-        election_results = self._county_block[cnty == self._county_block['county']][['cand1_percent', 'cand2_percent', 'cand3_percent', 'year']]        
+    def get_county(self, cnty, state):
+        election_results = self._county_block[cnty == self._county_block['county']]
+        election_results = election_results[state == self._county_block['state']][['cand1_percent', 'cand2_percent', 'cand3_percent', 'year']]        
         census_data = self._census_data[cnty == self._census_data['cnty_name']]
+        census_data = census_data[state == self._census_data['state_name']]
     
         census_year = census_data['year'].to_numpy()
         years = election_results['year'].to_numpy()
@@ -32,7 +34,7 @@ class Presidential_Results(object):
 
         census_data = census_data.loc[census_data['year'].isin(years)]
         census_data = census_data.drop(columns=['state', 'county', 'state_fips', 'cnty_fips', 'state_name', 'cnty_name', 'year'])
-        census_data = census_data.to_numpy()
+        
         
         winners = np.argmax(election_results, axis=1)
         label = np.zeros((election_results.shape[0], 1))
@@ -74,16 +76,18 @@ class Senate_Result(object):
 
     @property
     def counties(self):
-        return self._county_block['county'].to_list()
+        return [tuple(r) for r in self._county_block[['county', 'state']].to_numpy()]
 
     @property
     def states(self):
         return self._state_block['state'].to_list()
 
-    def get_county(self, cnty):
-        election_results = self._county_block[cnty == self._county_block['county']][['cand1_percent', 'cand2_percent', 'cand3_percent', 'year']]        
+    def get_county(self, cnty, state):
+        election_results = self._county_block[cnty == self._county_block['county']]
+        election_results = election_results[state == self._county_block['state']][['cand1_percent', 'cand2_percent', 'cand3_percent', 'year']]        
         census_data = self._census_data[cnty == self._census_data['cnty_name']]
-    
+        census_data = census_data[state == self._census_data['state_name']]
+
         census_year = census_data['year'].to_numpy()
         years = election_results['year'].to_numpy()
 
@@ -92,7 +96,6 @@ class Senate_Result(object):
 
         census_data = census_data.loc[census_data['year'].isin(years)]
         census_data = census_data.drop(columns=['state', 'county', 'state_fips', 'cnty_fips', 'state_name', 'cnty_name', 'year'])
-        census_data = census_data.to_numpy()
         
         winners = np.argmax(election_results, axis=1)
         label = np.zeros((election_results.shape[0], 1))
@@ -109,10 +112,25 @@ class Senate_Result(object):
     def get_state(self, state):
         census_data = self._census_data[state == self._census_data['state_name']]
 
+def preprocess_presidential_results():
+    _dataloader = Presidential_Results()
+    for county, state in _dataloader.counties:
+        _dataloader.get_county(county, state)
+        _dataloader.get_state(state)
+
+    
+def preprocess_senate_results():
+    _dataloader = Senate_Result()
+    for county, state in _dataloader.counties:
+        _dataloader.get_county(county, state)
+        _dataloader.get_state(state)
+
+
+
 if __name__ == "__main__":
     pres_dataloader = Presidential_Results()
-    pres_dataloader.get_county("Alcona")
+    pres_dataloader.get_county("Alcona", "MI")
     pres_dataloader.get_state('MI')
     sen_dataloader = Senate_Result()
-    sen_dataloader.get_county("Alcona")
+    sen_dataloader.get_county("Alcona", "MI")
     sen_dataloader.get_state('MI')    
